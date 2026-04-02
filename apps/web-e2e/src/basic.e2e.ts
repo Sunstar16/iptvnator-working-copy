93b65baf583375fe94dc6c354b1220b364de90e1
Sunstar_16 <68480330+Sunstar16@users.noreply.github.com>
2026-04-02 23:08:03 +0100

    Add files via upload

---
+++ apps/web-e2e/src/basic.e2e.ts
@@ -0,0 +1,22 @@
+import { expect, test } from '@playwright/test';
+import { join } from 'path';
+
+const fixturePath = join(__dirname, 'fixtures/test.m3u');
+
+test('basic test', async ({ page }) => {
+    await page.goto('/');
+
+    // Basic checks
+    expect(await page.title()).toBe('IPTVnator');
+
+    // Upload playlist test
+    await page.getByRole('button', { name: 'Add playlist' }).click();
+    const dialog = page.locator('mat-dialog-container');
+    await dialog.getByRole('button', { name: 'Add via file upload' }).click();
+    await page.setInputFiles('input[type="file"]', fixturePath);
+    await page.waitForURL(/\/workspace\/playlists\/.+\/all$/);
+    await expect(page.getByText('test.m3u')).toBeVisible();
+    await expect(page.getByText('4 channels')).toBeVisible();
+    await expect(page.getByText('1. Channel 1')).toBeVisible();
+    await expect(page.getByText('4. HappyKids TV')).toBeVisible();
+});
